00e78e9f18e96029f5a11af32c1986c61bcacb1a
chuber2024 <chris.huber@hotmail.de>
2024-09-17 16:48:01 +0200

    create subnets

--- networks.tf
+++ networks.tf
@@ -11,11 +11,11 @@ resource "azurerm_virtual_network" "vnet" {
     Workload    = var.az-rg[4]
   }
 }
-/* 
+
 resource "azurerm_subnet" "vprsubnet" {
   for_each             = var.private_subnets
   name                 = each.key
   resource_group_name  = "rg-${var.env}-${var.az-rg[4]}"
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.100.${each.value}.0/24"]
-} */
+}
--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,10 @@ output "Workspace" {
 output "StorageAccount-Diag" {
   value = azurerm_storage_account.storacc.primary_blob_endpoint
 }
-/* 
+ 
 output "Subnet-Name" {
   value = values(azurerm_subnet.vprsubnet)[*].name
 }
 output "Subnet-IDs" {
   value = values(azurerm_subnet.vprsubnet)[*].id
 }
- */
