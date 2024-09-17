379007b3ed3297ec20da2125c32a9dc05981b5d4
chuber2024 <chris.huber@hotmail.de>
2024-09-17 16:43:04 +0200

    next try create vnet

--- outputs.tf
+++ outputs.tf
@@ -9,10 +9,11 @@ output "Workspace" {
 output "StorageAccount-Diag" {
   value = azurerm_storage_account.storacc.primary_blob_endpoint
 }
-
+/* 
 output "Subnet-Name" {
   value = values(azurerm_subnet.vprsubnet)[*].name
 }
 output "Subnet-IDs" {
   value = values(azurerm_subnet.vprsubnet)[*].id
 }
+ */
